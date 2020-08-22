90e6606f1c5a4d9d3f80674fe1461445fb8f9151
tfunk2 <tyler.j.funk2@gmail.com>
2020-08-22 03:42:29 -0600

    cleared out boilerplate code and inserted title and logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PrintaBullLogo from './images/PrintaBull-Logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img className="printabull-logo" src={PrintaBullLogo} alt="PrintaBull logo"></img>
+        <h1 className="welcome-h1">Welcome to PrintaBull</h1>
       </header>
+      <body className="app-body">
+
+      </body>
     </div>
   );
 }
